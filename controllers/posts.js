d9e8a9be27edf2d6115c25df71aa8ba0219c1baa
Hope-Ray <anuragshetty007@gmail.com>
2023-02-22 20:18:28 +0530

    part 2

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,39 +1,76 @@
+import express from "express";
 import mongoose from "mongoose";
+
 import PostMessage from "../models/postMessage.js";
 
+const router = express.Router();
 
 export const getPosts = async (req, res) => {
+    const { page } = req.query;
+
     try {
-        const postMessage = await PostMessage.find(); 
-        console.log(postMessage);
-        res.status(200).json(postMessage);
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; //get the start index of the every page
+        const total = await PostMessage.countDocuments({});
+
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex); 
+        
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
     } catch (error) {
         res.status(404).json({ message :error.message});
     }
  }
 
+ export const getPostsBySearch = async (req, res) => {
+    const {searchQuery, tags} = req.query;
+    
+    try {
+        const title = new RegExp(searchQuery, "i");
+
+        const posts = await PostMessage.find({ $or: [ { title}, { tags: {$in: tags.split(',')} } ] });
+
+        res.json( {data: posts});
+    } catch (error) {
+        res.status(404).json({ message:error.message });
+    }
+}
+
+export const getPost = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id); 
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message :error.message});
+    }
+ }
 
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPost = new PostMessage(post);
+    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString()});
 
     try {
-        await newPost.save();
+        await newPostMessage.save();
 
-        res.status(201).json(newPost);
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });        
     }
 }
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;   //ID coming from URL eg: /posts/123  mentioned in routes
-    const post = req.body;
+    const { id } = req.params;   //ID coming from URL eg: /posts/123  mentioned in routes
+    const {title, message, creator, selectedFile, tags, locations } = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('NO post available with that ID');
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('NO post available with that ID');
     
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    const updatedPost = {title, message, creator, selectedFile, tags, locations, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+    
     res.json(updatedPost);
 }
 
@@ -44,19 +81,30 @@ export const deletePost = async (req, res) => {
     
     await PostMessage.findByIdAndRemove(id);
 
-    console.log('DELETE');
-
     res.json({message: "Post is deleted Successfully"});
 }
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if(!req.userId) return res.json({ message: 'Unauthenticated'})
+
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post available with that id');
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await  PostMessage.findByIdAndUpdate(id,{ likeCount: post.likeCount + 1 }, {new: true});
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if(index === -1){
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id != String(req.userId)); 
+    }
+
+    const updatedPost = await  PostMessage.findByIdAndUpdate(id, post , {new: true});
 
     res.json(updatedPost);
 
 }
+
+export default router;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30MB", extended: "true"}));
 app.use(cors());
 
 app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
 
 //const CONNECTION_URL = "mongodb+srv://anurag123:anurag123@cluster0.dcw3ieo.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
